a6951d08358cdae7ab9707c3245ef89198e0d5a6
Jef Mitchell <jef@project87.net>
2021-10-22 13:24:38 -0400

    created userRoutes with comments for direction

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// POST '/signup' create new account
+
+// POST '/login' login page
+
+// POST '/logout' logout page
+
+module.exports = router;
