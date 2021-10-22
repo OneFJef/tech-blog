075859e67b2d023b09bfa2d84f9d2f9fdf24fe66
Jef Mitchell <jef@project87.net>
2021-10-22 13:24:24 -0400

    created postRoutes with comment for direction

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// POST '/' (check login)
+
+// UPDATE '/:id' (check login)
+
+// DELETE '/:id' (check login)
+
+module.exports = router;
