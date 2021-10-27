f3ac301fe6e585eefb9fcc2e1a0af25e246a7e33
Jef Mitchell <jef@project87.net>
2021-10-27 10:50:04 -0400

    Created user route with login and log out.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,17 +1,68 @@
-const router = require('express').Router();
-const { READUNCOMMITTED } = require('sequelize/types/lib/table-hints');
-const { User } = require('../../models');
+const router = require("express").Router();
+const { READUNCOMMITTED } = require("sequelize/types/lib/table-hints");
+const { User } = require("../../models");
 
-// GET '/:id' find user and fetch all user post (pagination)
+// Create an account.
+router.post("/", async (req, res) => {
+  try {
+    const { user_name, email, password } = req.body;
+    if (!user_name || !email || !password)
+      res
+        .status(400)
+        .send("Please enter a User Name, E-Mail, and Password to sign up.");
+    const userAccount = User.create({ user_name, email, password });
+    req.session.save(() => {
+      req.session.user_id = userAccount.id;
+      req.session.logged_in = true;
 
-// UPDATE '/:id' update user password
+      res.status(200).json(userAccount);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-// 
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const userAccount = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userAccount) {
+      res
+        .status(400)
+        .json({ message: "Invalid credentials, please try again." });
+      return;
+    }
+
+    const validPassword = await userAccount.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Invalid credentials, please try again." });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userAccount.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userAccount, message: "Login Successful." });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Logout
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
-
-// Create
-// Read One
-// Read All
-// Update
-// Delete
