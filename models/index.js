7cdc8edd76769a2879ade04c433b6515401ea16a
Jef Mitchell <jef@project87.net>
2021-10-27 10:50:16 -0400

    Added comment model.

--- models/index.js
+++ models/index.js
@@ -1,8 +1,35 @@
-const User = require("./User");
+const Comment = require("./Comment");
 const Post = require("./Post");
+const User = require("./User");
 
-// User has many Posts
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
 
-// Post belongs to a single user
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
 
-module.exports = { User, Post };
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+module.exports = { Comment, Post, User };
