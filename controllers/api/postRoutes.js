a6b390ffa2152a9ad5434acd84d6d945dd64a6c6
Jef Mitchell <jef@project87.net>
2021-10-27 10:49:25 -0400

    Added comment

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// GET '/' All posts
+
+
 // POST '/:id' (check login)
 
 // UPDATE '/:id' (check login)
